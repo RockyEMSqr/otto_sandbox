1787b4ba45e71c1e65df51b1b2a2e1d6a5387712
Rocky <rocco.cutri@emsquared-inc.com>
2022-08-22 09:27:23 -0600

    vite and parcel2 tests. parcel2 no go

---
+++ parcel2/main.tsx
@@ -0,0 +1,7 @@
+import { render } from 'preact';
+export const App = () => {
+    return <h1>Hello</h1>;
+}
+
+render(<App />, document.body);
+// alert('Hi')
---
+++ parcel2/main1.js
@@ -0,0 +1,7 @@
+import { render, h } from 'preact';
+export const App = () => {
+    return <h1>Hello from Js</h1>
+}
+render(<App />, document.body);
+
+alert('Hi')
